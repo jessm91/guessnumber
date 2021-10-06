3c4430fa3a39bf6f01e4d2e838ef05df62063695
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-06 10:16:52 -0500

    added while loop to keep guessing

--- guessnumber.py
+++ guessnumber.py
@@ -1,13 +1,16 @@
 from random import randint
 from random import choice
 
-"""Generate random number between 1 and 100"""
-numb = randint(1, 100)
+"""Generate random number between 1 and 10"""
+numb = randint(1, 10)
 
-"""Ask user to guess a number"""
-guess = input("Guess a number between 1 and 100: ")
-
-if numb == guess:
-    print("You have guessed the correct number!")
-else:
-    print("Wrong!")
+while True:
+    """Ask user to guess a number"""
+    guess = input("Guess a number between 1 and 10 [enter 'q' to quit]: ")
+    guess = int(guess)
+    if numb == guess:
+        print("You have guessed the correct number!")
+        break
+    else:
+        print("Wrong! Guess again")
+        continue
