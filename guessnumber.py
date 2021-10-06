7939827d81f83d04cd881f6f4153d11002fb4f51
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-06 10:18:59 -0500

    added quit option

--- guessnumber.py
+++ guessnumber.py
@@ -7,7 +7,12 @@ numb = randint(1, 10)
 while True:
     """Ask user to guess a number"""
     guess = input("Guess a number between 1 and 10 [enter 'q' to quit]: ")
+
+    if guess == 'q':
+        break
+        
     guess = int(guess)
+
     if numb == guess:
         print("You have guessed the correct number!")
         break
