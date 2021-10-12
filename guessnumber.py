d34ffd51c1ae3c51b265b77139b66dd49fb1e0ae
Jessica Mangiameli <jmangiam@asu.edu>
2021-10-12 10:20:52 -0500

    added counter

--- guessnumber.py
+++ guessnumber.py
@@ -1,7 +1,8 @@
 from random import randint
 
-"""Generate random number between 1 and 10"""
+#Generate random number between 1 and 10
 numb = randint(1, 10)
+guesses = 0
 
 while True:
     """Ask user to guess a number"""
@@ -11,6 +12,7 @@ while True:
         break
 
     guess = int(guess)
+    guesses = guesses + 1
 
     if numb == guess:
         print("You have guessed the correct number!")
@@ -18,3 +20,5 @@ while True:
     else:
         print("Wrong! Guess again")
         continue
+
+print(f"It took you {guesses} tries to guess the correct number!")
